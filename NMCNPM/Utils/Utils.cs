b222a69ea0d906aaf2037464c270570adc6ce9f9
Peter Nguyen <peternguyen@Peters-MacBook-Pro.local>
2014-12-30 09:46:45 +0700

    Adding Utils function

---
+++ NMCNPM/Utils/Utils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NMCNPM
+{
+    class Utils
+    {
+        // validate phone number
+        public Boolean validate_PhoneNumberInput(string phone_number)
+        {
+            Regex regex = new Regex(@"\d{9,11}");
+            Match match = regex.Match(phone_number);
+            Boolean isMatch = false;
+            if (match.Success)
+            {
+                // be sure that matched value must be equal to input value
+                // remove 123123123aaaaa123
+                if (match.Value == phone_number)
+                {
+                    isMatch = true;
+                }
+            }
+            return isMatch;
+        }
+
+        public string generatePrimaryKey(string prefix,int rows)
+        {
+            int temp = rows;
+            int count = 0;
+
+            if (rows > 9999)
+            {
+                return "-1";
+            }
+
+            // get length of number
+            while (temp > 0)
+            {
+                count++;
+                temp /= 10;
+            }
+            //padding zero
+            for (int i = 0; i < (4 - count); i++)
+            {
+                prefix += "0";
+            }
+            prefix += rows.ToString();
+            return prefix;
+        }
+    }
+}
